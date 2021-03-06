550a7c20aac6952c94ec317bf31a0f2e46e069db
Hemachandhiran Harimoorthy <hemanth089@gmail.com>
2021-03-06 09:45:55 +0530

    Added Translator Commands

---
+++ src/Feature/Translator/code/Commands/TranslatorDialog.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+
+namespace StrikersInfy.Feature.Translator.Commands
+{
+    public class TranslatorDialog : Sitecore.Shell.Framework.Commands.Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Context.ClientPage.Start(this, "Run", context.Parameters);
+        }
+
+        protected static void Run(ClientPipelineArgs args)
+        {
+            if (!args.IsPostBack)
+            {
+                UrlString urlString = new UrlString(UIUtil.GetUri("control:TranslatorDialog"));
+                SheerResponse.ShowModalDialog(urlString.ToString(), "800", "300", "", true);
+                args.WaitForPostBack();
+            }
+            else
+            {
+                if (args.HasResult)
+                {
+                    if (Sitecore.Context.Item.Name == "Content Editor")
+                    {
+                        Sitecore.Context.ClientPage.ClientResponse.SetLocation(Sitecore.Links.LinkManager.GetItemUrl(Sitecore.Context.Item));
+                    }
+                }
+            }
+        }
+    }
+}
