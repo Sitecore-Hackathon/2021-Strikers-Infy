1d3cca7cedce83bc80522300cd8ebb219366ebe5
Hemachandhiran Harimoorthy <hemanth089@gmail.com>
2021-03-06 13:57:52 +0530

    Transalation functionality completed

---
+++ src/Feature/Translator/code/Models/ContentProps.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StrikersInfy.Feature.Translator.Models
+{
+    public class ContentProps
+    {
+        public string LangCode { get; set; }
+        public string ContentText { get; set; }
+    }
+}
---
+++ src/Feature/Translator/code/Pipelines/RegisterRoutes.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace StrikersInfy.Feature.Translator.Pipelines
+{
+    public class RegisterRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            HttpConfiguration config = GlobalConfiguration.Configuration;
+
+            Register(config);
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            //config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "TranslatorApi",
+                routeTemplate: "api/Translator/{action}",
+                new { controller = "Translator" }
+            );           
+        }
+    }
+}
